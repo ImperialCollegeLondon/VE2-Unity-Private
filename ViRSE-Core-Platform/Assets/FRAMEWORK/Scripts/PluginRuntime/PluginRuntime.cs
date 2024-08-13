7b07f351381216feba45292372256e8041128a98
Fred Tovey-Ansell <freddyta99@gmail.com>
2024-08-13 13:31:41 +0100

    Fix test, remove duplicate script

--- ViRSE-Core-Platform/Assets/FRAMEWORK/Scripts/PluginRuntime/PluginRuntime.cs
+++ ViRSE-Core-Platform/Assets/FRAMEWORK/Scripts/PluginRuntime/PluginRuntime.cs
@@ -1,22 +1,66 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
+using ViRSE.FrameworkRuntime;
 
-[Serializable]
-public class PluginConfig
+namespace ViRSE.PluginRuntime
 {
-    public UnityEvent<int> OnRemoteUserJoinInstance;
-    public UnityEvent<int> OnRemoteUserLeavenInstance;
-}
-
-public class PluginRuntime : MonoBehaviour
-{
-    private PluginConfig _pluginConfig;
-
-    public void Initialize(PluginConfig pluginConfig)
+    //TODO - this handles setting up the player, and setting up the plugin networking stuff, maybe these should be separated out? Both would need a server type
+    public class PluginRuntime
     {
-        _pluginConfig = pluginConfig;
+        public static PluginRuntime Instance;
+
+        //Refences to framework interfaces
+        private IFrameworkRuntime _frameworkRuntime;
+
+        //References to the Plugin Subservices
+        private PluginSyncService _pluginSyncService;
+
+       public ServerType ServerType;
+
+
+        //private PluginPrimaryUIService _pluginPrimaryUIService
+        //private PluginSecondaryUIService _pluginSecondaryUIService
+
+        private Transform _playerSpawnTransform;
+
+        public PluginRuntime(IFrameworkRuntime frameworkRuntime, ServerType serverType, Transform playerSpawnTransform)
+        {
+            Instance = this;
+            ServerType = serverType;
+            _frameworkRuntime = frameworkRuntime;
+
+            if (serverType != ServerType.Offline)
+                _pluginSyncService = new(frameworkRuntime.PrimaryServerService);
+
+            if (frameworkRuntime.IsFrameworkReady)
+                HandleFrameworkReady();
+            else
+                frameworkRuntime.OnFrameworkReady += HandleFrameworkReady;
+
+            _playerSpawnTransform = playerSpawnTransform;
+        }
+
+        private void HandleFrameworkReady()
+        {
+            MovePlayerToPluginStartingPosition();
+        }
+
+        private void MovePlayerToPluginStartingPosition()
+        {
+            _frameworkRuntime.LocalPlayerRig.Position = _playerSpawnTransform.position;
+            _frameworkRuntime.LocalPlayerRig.Rotation = _playerSpawnTransform.rotation;
+        }
+
+        public void HandleNetworkUpdate()
+        {
+            _pluginSyncService.HandleNetworkUpdate();
+        }
+
+        public void TearDown()
+        {
+            _frameworkRuntime.OnFrameworkReady -= HandleFrameworkReady;
+            _pluginSyncService.TearDown();
+        }
     }
 }
