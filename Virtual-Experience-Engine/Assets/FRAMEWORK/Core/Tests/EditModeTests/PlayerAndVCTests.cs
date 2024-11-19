68c1014075c73bc8fc47cf4fdf1141f61a88ad73
sventure <sidswamy2002@gmail.com>
2024-11-19 16:47:38 +0000

    setupfixtures for each setup call in the current namespace

--- Virtual-Experience-Engine/Assets/FRAMEWORK/Core/Tests/EditModeTests/PlayerAndVCTests.cs
+++ Virtual-Experience-Engine/Assets/FRAMEWORK/Core/Tests/EditModeTests/PlayerAndVCTests.cs
@@ -1,92 +1,86 @@
 using NSubstitute;
 using NUnit.Framework;
-using VE2.Core.Player;
 using System;
 using VE2.Core.VComponents.Tests;
 using VE2.Core.VComponents.PluginInterfaces;
 using UnityEngine;
-using VE2.Common;
-using static VE2.Common.CommonSerializables;
 using VE2.Core.VComponents.InteractableFindables;
-using VE2.Core.VComponents.Internal;
+
 
 namespace VE2.Core.Tests
 {
-    public class PushActivatableTests
+    [TestFixture]
+    [Category("Player and ToggleActivatable Tests")]
+    public class PlayerAndToggleActivatableTests
     {
+        //variables that will be reused in the tests
+        private IV_ToggleActivatable _activatablePluginInterface;
+        private PluginScriptMock _customerScript;
+        private V_ToggleActivatableStub _v_activatableStub;
+        private IRangedClickPlayerInteractableIntegrator _activatableRaycastInterface;
+        private IInputHandler _inputHandlerStub;
+        private ushort _localClientID;
+
+        //Setup Once for every single test in this test fixture
+        [OneTimeSetUp]
+        public void SetUpOnce()
+        {
+            //Create the activatable
+            _v_activatableStub = VComponentStubFactory.CreateToggleActivatableStub();
+            _activatablePluginInterface = _v_activatableStub;
+            _activatableRaycastInterface = _v_activatableStub;
+
+            RayCastProviderSetup.SetUpRaycastProviderStub(_activatableRaycastInterface.RangedClickInteractionModule);
+            PlayerServiceSetup.SetUpPlayerServiceStub();
+
+            _inputHandlerStub = InputHandlerSetup.InputHandlerStub;
+            _localClientID = MultiplayerSupportSetup.InteractorID.ClientID;
+        }
+
+        //setup that runs before every test method in this test fixture
+        [SetUp]
+        public void SetUpBeforeEveryTest()
+        {
+            //Wire up the customer script to receive the events           
+            _customerScript = Substitute.For<PluginScriptMock>();
+            _activatablePluginInterface.OnActivate.AddListener(_customerScript.HandleActivateReceived);
+            _activatablePluginInterface.OnDeactivate.AddListener(_customerScript.HandleDeactivateReceived);
+        }
+
+        //test method to confirm that the activatable emits the correct events when the player interacts with it
         [Test]
         public void OnUserClick_WithHoveringActivatable_CustomerScriptReceivesOnActivate()
         {
-            //Create an activatable with default config and state
-            ToggleActivatableService toggleActivatable = new( 
-                new ToggleActivatableConfig(),
-                new SingleInteractorActivatableState(),
-                "testID",
-                new WorldStateModulesContainer()
-            );
-
-            //Stub out the VC (integration layer) with the activatable
-            V_ToggleActivatableStub v_activatableStub = new(toggleActivatable);
-
-            //Get interfaces
-            IV_ToggleActivatable activatablePluginInterface = v_activatableStub;
-            IRangedClickPlayerInteractableIntegrator activatableRaycastInterface = v_activatableStub;
-
-            //Stub out the player settings provider with default settings
-            IPlayerSettingsProvider playerSettingsProviderStub = Substitute.For<IPlayerSettingsProvider>();
-            playerSettingsProviderStub.UserSettings.Returns(new UserSettingsPersistable());
-
-            //Stub out the multiplayer support
-            System.Random random = new();
-            ushort localClientID = (ushort)random.Next(0, ushort.MaxValue);
-            IMultiplayerSupport multiplayerSupportStub = Substitute.For<IMultiplayerSupport>();
-            multiplayerSupportStub.IsConnectedToServer.Returns(true);
-            multiplayerSupportStub.LocalClientID.Returns(localClientID);
-
-            //Stub out the input handler    
-            IInputHandler inputHandlerStub = Substitute.For<IInputHandler>();
-
-            //Stub out the raycast provider to hit the activatable GO with 0 range
-            IRaycastProvider raycastProviderStub = Substitute.For<IRaycastProvider>();
-            raycastProviderStub
-                .TryGetRangedInteractionModule(default, default, out Arg.Any<RaycastResultWrapper>(), default, default)
-                .ReturnsForAnyArgs(x =>
-                {
-                    x[2] = new RaycastResultWrapper(activatableRaycastInterface.RangedClickInteractionModule, 0);
-                    return true;
-                });
-
-            //Create the player (2d)
-            PlayerService playerService = new(
-                new PlayerTransformData(),
-                new PlayerStateConfig(),
-                false,
-                true,
-                new PlayerStateModuleContainer(),
-                playerSettingsProviderStub,
-                Substitute.For<IPlayerAppearanceOverridesProvider>(),
-                multiplayerSupportStub,
-                inputHandlerStub,
-                raycastProviderStub, 
-                Substitute.For<IXRManagerWrapper>()
-            );
-
-            //Wire up mock customer script
-            PluginScriptMock PluginScriptMock = Substitute.For<PluginScriptMock>();
-            activatablePluginInterface.OnActivate.AddListener(PluginScriptMock.HandleActivateReceived);
-            activatablePluginInterface.OnDeactivate.AddListener(PluginScriptMock.HandleDeactivateReceived);
-
             //Check customer received the activation, and that the interactorID is set
-            inputHandlerStub.OnMouseLeftClick += Raise.Event<Action>();
-            PluginScriptMock.Received(1).HandleActivateReceived(); 
-            Assert.IsTrue(activatablePluginInterface.IsActivated);
-            Assert.AreEqual(activatablePluginInterface.MostRecentInteractingClientID, localClientID);
+            _inputHandlerStub.OnMouseLeftClick += Raise.Event<Action>();
+            _customerScript.Received(1).HandleActivateReceived();
+            Assert.IsTrue(_activatablePluginInterface.IsActivated, "Activatable should be activated");
+            Assert.AreEqual(_activatablePluginInterface.MostRecentInteractingClientID, _localClientID);
 
             // Invoke the click to deactivate
-            inputHandlerStub.OnMouseLeftClick += Raise.Event<Action>();
-            PluginScriptMock.Received(1).HandleDeactivateReceived();
-            Assert.IsFalse(activatablePluginInterface.IsActivated);
-            Assert.AreEqual(activatablePluginInterface.MostRecentInteractingClientID, localClientID);
+            _inputHandlerStub.OnMouseLeftClick += Raise.Event<Action>();
+            _customerScript.Received(1).HandleDeactivateReceived();
+
+            Assert.IsFalse(_activatablePluginInterface.IsActivated, "Activatable should be deactivated");
+            Assert.AreEqual(_activatablePluginInterface.MostRecentInteractingClientID, _localClientID);
+        }
+
+        //tear down that runs after every test method in this test fixture
+        [TearDown]
+        public void TearDownAfterEveryTest()
+        {
+            _customerScript.ClearReceivedCalls();
+            _activatablePluginInterface.IsActivated = false;
+        }
+
+        //tear down that runs once after all the tests in this class
+        [OneTimeTearDown]
+        public void TearDownOnce()
+        {
+            _activatablePluginInterface.OnActivate.RemoveAllListeners();
+            _activatablePluginInterface.OnDeactivate.RemoveAllListeners();
+
+            _v_activatableStub.TearDown();
         }
     }
 }
---
+++ Virtual-Experience-Engine/Assets/FRAMEWORK/Core/Tests/EditModeTests/PlayerTestSetUpFixtures.cs
@@ -0,0 +1,143 @@
+using NUnit.Framework;
+using NSubstitute;
+using VE2.Common;
+using static VE2.Common.CommonSerializables;
+using VE2.Core.VComponents.InteractableInterfaces;
+using VE2.Core.Player;
+using UnityEngine;
+
+namespace VE2.Core.Tests
+{
+    //public class PlayerSetup { }
+
+    [SetUpFixture]
+    public class PlayerSettingsProviderSetup
+    {
+        public static IPlayerSettingsProvider PlayerSettingsProviderStub { get; private set; }
+
+        [OneTimeSetUp]
+        public void PlayerSettingsProviderStubSetupOnce()
+        {
+            //Stub out the player settings provider with default settings
+            PlayerSettingsProviderStub = Substitute.For<IPlayerSettingsProvider>();
+            PlayerSettingsProviderStub.UserSettings.Returns(new UserSettingsPersistable());
+        }
+    }
+
+    [SetUpFixture]
+    public class InputHandlerSetup
+    {
+        public static IInputHandler InputHandlerStub { get; private set; }
+
+        [OneTimeSetUp]
+        public void InputHandlerStubSetupOnce()
+        {
+            //Stub out the input handler    
+            InputHandlerStub = Substitute.For<IInputHandler>();
+        }
+    }
+    
+    [SetUpFixture]
+    public class MultiplayerSupportSetup
+    {
+        public static IMultiplayerSupport MultiplayerSupportStub { get; private set; }
+        public static InteractorID InteractorID { get; private set; }
+        public static string InteractorGameobjectName { get; private set; }
+
+        [OneTimeSetUp]
+        public void MultiplayerSupportStubSetupOnce()
+        {
+            //Stub out the multiplayer support
+            System.Random random = new();
+            ushort localClientID = (ushort)random.Next(0, ushort.MaxValue);
+
+            MultiplayerSupportStub = Substitute.For<IMultiplayerSupport>();
+            MultiplayerSupportStub.IsConnectedToServer.Returns(true);
+            MultiplayerSupportStub.LocalClientID.Returns(localClientID);
+
+            InteractorID = new(localClientID, InteractorType.Mouse2D);
+            InteractorGameobjectName = $"Interactor{InteractorID.ClientID}-{InteractorID.InteractorType}";
+        }
+    }
+
+    [SetUpFixture]
+    public class InteractorSetup
+    {
+        public static IInteractor InteractorStub { get; private set; }
+        public static GameObject InteractorGameObject { get; private set; }
+
+        [OneTimeSetUp]
+        public void InteractorStubSetupOnce()
+        {
+            InteractorStub = Substitute.For<IInteractor>();
+            InteractorGameObject = new();
+        }
+    }
+
+    [SetUpFixture]
+    public class GameObjectFindProviderSetup
+    {
+        public static IGameObjectFindProvider GameObjectFindProviderStub { get; private set; }
+
+        [OneTimeSetUp]
+        public void GameObjectFindProviderStubSetupOnce()
+        {
+            string interactorGameobjectName = MultiplayerSupportSetup.InteractorGameobjectName;
+            IInteractor interactorStub = InteractorSetup.InteractorStub;
+            GameObject interactorGameObject = InteractorSetup.InteractorGameObject;
+
+            //Stub out the game object find provider
+            GameObjectFindProviderStub = Substitute.For<IGameObjectFindProvider>();
+            GameObjectFindProviderStub.FindGameObject(MultiplayerSupportSetup.InteractorGameobjectName).Returns(interactorGameObject);
+            GameObjectFindProviderStub.TryGetComponent<IInteractor>(interactorGameObject, out Arg.Any<IInteractor>()).Returns(x =>
+            {
+                x[1] = interactorStub;
+                return true;
+            });
+        }
+    }
+
+    [SetUpFixture]
+    public class RayCastProviderSetup
+    {
+        public static IRaycastProvider RaycastProviderStub { get; private set; }
+
+        public static void SetUpRaycastProviderStub(IRangedInteractionModule rangedInteractionModule)
+        {
+            //Stub out the raycast provider to return the provided interaction module
+            RaycastProviderStub = Substitute.For<IRaycastProvider>();
+            RaycastProviderStub
+                .TryGetRangedInteractionModule(default, default, out Arg.Any<RaycastResultWrapper>(), default, default)
+                .ReturnsForAnyArgs(x =>
+                {
+                    x[2] = new RaycastResultWrapper(rangedInteractionModule, 0);
+                    return true;
+                });
+        }
+    }
+
+    [SetUpFixture]
+    public class PlayerServiceSetup
+    {
+        public static PlayerService PlayerServiceStub { get; private set; }
+
+        public static void SetUpPlayerServiceStub()
+        {
+            //Create the player (2d)
+            PlayerServiceStub = new(
+                new PlayerTransformData(),
+                new PlayerStateConfig(),
+                false,
+                true,
+                new PlayerStateModuleContainer(),
+                PlayerSettingsProviderSetup.PlayerSettingsProviderStub,
+                Substitute.For<IPlayerAppearanceOverridesProvider>(),
+                MultiplayerSupportSetup.MultiplayerSupportStub,
+                InputHandlerSetup.InputHandlerStub,
+                RayCastProviderSetup.RaycastProviderStub, 
+                Substitute.For<IXRManagerWrapper>()
+            );
+        }
+    }
+}
+
