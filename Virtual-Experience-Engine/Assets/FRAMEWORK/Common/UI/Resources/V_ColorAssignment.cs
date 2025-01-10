709efbc5010538f93cfc10ecd507437873e61a29
Fred Tovey-Ansell <freddyta99@gmail.com>
2025-01-10 15:23:52 +0000

    Progress on example UI, import more UI assets, 9-slicing

--- Virtual-Experience-Engine/Assets/FRAMEWORK/Common/UI/Resources/V_ColorAssignment.cs
+++ Virtual-Experience-Engine/Assets/FRAMEWORK/Common/UI/Resources/V_ColorAssignment.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,23 +15,34 @@ public class V_ColorAssignment : MonoBehaviour
         AccentSecondary
     }
 
-    [SerializeField] private ColorType _colorType;
+    private enum ButtonType
+    {
+        Standard, 
+        Secondary
+    }
+
+    [SerializeField, HideIf(nameof(_hasButton), true)] private ColorType _colorType;
+    [SerializeField, HideIf(nameof(_hasButton), false)] private ButtonType _buttonType;
+
+    private bool _hasButton => _button != null; 
 
     private Image _image;
+    private TMP_Text _text;
+    private Button _button;
+
     private ColorConfiguration _colorConfiguration;
 
     private void Awake()
     {
         _image = GetComponent<Image>();
+        _text = GetComponent<TMP_Text>();
+        _button = GetComponent<Button>();
         _colorConfiguration = (ColorConfiguration)Resources.Load("ColorConfiguration");
         UpdateColor();
     }
 
     private void OnValidate()
     {
-        if (_image == null)
-            return;
-
         UpdateColor();
     }
 
@@ -39,22 +51,65 @@ public class V_ColorAssignment : MonoBehaviour
         switch (_colorType)
         {
             case ColorType.Primary:
-                _image.color = _colorConfiguration.PrimaryColor;
+                if (_image != null && !_hasButton)
+                    _image.color = _colorConfiguration.PrimaryColor;
+                if (_text != null)
+                    _text.color = _colorConfiguration.PrimaryColor;
                 break;
             case ColorType.Secondary:
-                _image.color = _colorConfiguration.SecondaryColor;
+                if (_image != null && !_hasButton)
+                    _image.color = _colorConfiguration.SecondaryColor;
+                if (_text != null)
+                    _text.color = _colorConfiguration.SecondaryColor;
                 break;
             case ColorType.Tertiary:
-                _image.color = _colorConfiguration.TertiaryColor;
+                if (_image != null && !_hasButton)
+                    _image.color = _colorConfiguration.TertiaryColor;
+                if (_text != null)
+                    _text.color = _colorConfiguration.TertiaryColor;
                 break;
             case ColorType.Quaternary:
-                _image.color = _colorConfiguration.QuaternaryColor;
+                if (_image != null && !_hasButton)
+                    _image.color = _colorConfiguration.QuaternaryColor;
+                if (_text != null)
+                    _text.color = _colorConfiguration.QuaternaryColor;
                 break;
             case ColorType.AccentPrimary:
-                _image.color = _colorConfiguration.AccentPrimaryColor;
+                if (_image != null && !_hasButton)
+                    _image.color = _colorConfiguration.AccentPrimaryColor;
+                if (_text != null)
+                    _text.color = _colorConfiguration.AccentPrimaryColor;
                 break;
             case ColorType.AccentSecondary:
-                _image.color = _colorConfiguration.AccentSecondaryColor;
+                if (_image != null && !_hasButton)
+                    _image.color = _colorConfiguration.AccentSecondaryColor;
+                if (_text != null)
+                    _text.color = _colorConfiguration.AccentSecondaryColor;
+                break;
+        }
+
+        switch (_buttonType)
+        {
+            case ButtonType.Standard:
+                if (_button != null)
+                    _button.colors = new ColorBlock
+                    {
+                        //TODO: add explicit button colors to colour config
+                        normalColor = _colorConfiguration.SecondaryColor,
+                        highlightedColor = _colorConfiguration.AccentSecondaryColor,
+                        pressedColor = _colorConfiguration.AccentSecondaryColor,
+                        selectedColor = _colorConfiguration.AccentPrimaryColor,
+                        disabledColor = _colorConfiguration.SecondaryColor,
+                        colorMultiplier = 1,
+                        fadeDuration = 0.1f
+                    };
+                break;
+            case ButtonType.Secondary:
+                if (_button != null)
+                    _button.colors = new ColorBlock
+                    {
+                        
+                    };
                 break;
         }
     }
