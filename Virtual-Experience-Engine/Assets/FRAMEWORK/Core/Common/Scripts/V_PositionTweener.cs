a377cfa038bb1cee6d370be7982911f2d9b08aa7
Fred Tovey-Ansell <freddyta99@gmail.com>
2025-03-22 19:21:24 +0000

    Add some tweening utils for the sample scene

---
+++ Virtual-Experience-Engine/Assets/FRAMEWORK/Core/Common/Scripts/V_PositionTweener.cs
@@ -0,0 +1,51 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class V_PositionTweener : MonoBehaviour
+{
+    [Title("Values")]
+    [BeginGroup, SerializeField] private Vector3 _minPosition;
+    [SerializeField] private Quaternion _minRotation;
+
+    [SerializeField] private Vector3 _maxPosition;
+    [EditorButton(nameof(CaptureStartValues), "Capture Start Values", Order = 110)]
+    [EditorButton(nameof(CaptureEndValues), "Capture End Values", Order = 110)]
+    [EndGroup, SerializeField] private Quaternion _maxRotation;
+
+    [SerializeField, SpaceArea(spaceBefore: 5)] private float _duration;
+    [SerializeField] private Ease _easeType = Ease.Linear;
+
+    private Tween _currentPosTween;
+    private Tween _currentRotTween;
+
+    public void GoToMax()
+    {
+        Debug.Log("GoToMax"); //being called, _maxPosition is set, but the transform wont move!
+        _currentPosTween?.Kill();
+        _currentPosTween = transform.DOMove(_maxPosition, _duration).SetEase(_easeType);
+
+        _currentRotTween?.Kill();
+        _currentRotTween = transform.DORotateQuaternion(_maxRotation, _duration).SetEase(_easeType);
+    }
+
+    public void GoToMin()
+    {
+        _currentPosTween?.Kill();
+        _currentPosTween = transform.DOMove(_minPosition, _duration).SetEase(_easeType);
+
+        _currentRotTween?.Kill();
+        _currentRotTween = transform.DORotateQuaternion(_minRotation, _duration).SetEase(_easeType);
+    }
+
+    private void CaptureStartValues()
+    {
+        _minPosition = transform.position;
+        _minRotation = transform.rotation;
+    }
+
+    private void CaptureEndValues()
+    {
+        _maxPosition = transform.position;
+        _maxRotation = transform.rotation;
+    }
+}
