2ad30cba094606136f71860bfcd429b3de02538b
Simon Pocock <32938450+srpocock@users.noreply.github.com>
2025-05-12 18:47:08 +0100

    Working NetworkObjectTest

--- Virtual-Experience-Engine/Assets/FRAMEWORK/Core/VComponents/Scripts/API/AssemblyInfo.cs
+++ Virtual-Experience-Engine/Assets/FRAMEWORK/Core/VComponents/Scripts/API/AssemblyInfo.cs
@@ -5,3 +5,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("VE2.Core.Player.Internal")] 
 [assembly: InternalsVisibleTo("VE2.Core.Tests")] 
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] //For NSubstitute substitutes
+[assembly: InternalsVisibleTo("VE2.NonCore.Instancing.VComponents.Tests")]
--- Virtual-Experience-Engine/Assets/FRAMEWORK/Non-Core/Instancing/Scripts/Tests/EditModeTests/NetworkObjectTests.cs
+++ Virtual-Experience-Engine/Assets/FRAMEWORK/Non-Core/Instancing/Scripts/Tests/EditModeTests/NetworkObjectTests.cs
@@ -1,33 +1,60 @@
 using NSubstitute;
 using NUnit.Framework;
+using System.Collections.Generic;
 using VE2.NonCore.Instancing.API;
 using VE2.NonCore.Instancing.Internal;
+using VE2.Core.VComponents.API;
+using VE2.Core.Common;
+
 namespace VE2.NonCore.Instancing.VComponents.Tests
 {
-    public class NetworkObjectTests
+    [TestFixture]
+    [Category("Network Object Tests")]
+    internal class NetworkObjectTests
     {
+
         [Test]
         public void NetworkObject_WhenObjectIsSet_EmitsToPlugin()
         {
             //Arrange=========
             //  Create the NetworkObjectService, injecting default configs 
-            //  Create a stub for the VC (MonoBehaviour integration layer), injecting the service (See PushActivatableTests for this)
+            NetworkObjectService networkObjectService = new( new NetworkObjectStateConfig(), new NetworkObjectState(), "test", Substitute.For<IWorldStateSyncService>());
+            //  Create a stub for the VC
+            V_NetworkObjectStub v_networkObjectStub = new(networkObjectService);
             //  Get the plugin-facing interface out of the VC 
+            IV_NetworkObject networkObjectInterface = v_networkObjectStub;
             //  Create a substitute for the PluginScript, wire it up to the plugin interface 
+            PluginScript customerScript = new();
+            networkObjectInterface.OnStateChange.AddListener(customerScript.HandleObjectReceived);
 
             //Act=========
             //  Programmatically set the network object 
+            System.Random random = new();
+            int serializableObject = random.Next(int.MinValue, int.MaxValue);
+            networkObjectInterface.NetworkObject = serializableObject;
 
-            //Assert=========
-            //  Check the plugin script mock reveived the same object back 
+            // ===========Assert=========
+            // Check the customer received the same object
+            Assert.IsInstanceOf<int>(customerScript.ReceivedObject);
+            Assert.AreEqual((int)customerScript.ReceivedObject, serializableObject);
+
+            // Check also that HandleObjectReceived was only called once
+            Assert.AreEqual(customerScript.ReceivedCounter, 1);
 
             //Later on, we'll think about how to do an intergration test, where we get the syncer to set the value on the network object 
         }
     }
 
-    public class PluginScript
+    internal class PluginScript
     {
-        public virtual void HandleObjectReceived(object obj) { } //Virtual so the method can be mocked (so we can assert it was called with the right object)
+        public object ReceivedObject;
+        public int ReceivedCounter = 0;
+
+        public virtual void HandleObjectReceived(object obj)
+        {
+            ReceivedObject = obj;
+            ReceivedCounter++;
+        }
     }
 
     internal class V_NetworkObjectStub : IV_NetworkObject //We can't Substitute.For a MonoBehaviour, so we create an explicit test double class instead 
