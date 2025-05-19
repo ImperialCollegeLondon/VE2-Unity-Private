4c5fd93c48f65048fb35b3ee16d4728dd14a0ad2
Fred Tovey-Ansell <freddyta99@gmail.com>
2025-05-19 17:00:11 +0100

    Error logging for syncable reg clashes

--- Virtual-Experience-Engine/Assets/FRAMEWORK/Common/Scripts/Shared/WorldStateSyncablesContainer.cs
+++ Virtual-Experience-Engine/Assets/FRAMEWORK/Common/Scripts/Shared/WorldStateSyncablesContainer.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 namespace VE2.Common.Shared
@@ -19,8 +20,44 @@ namespace VE2.Common.Shared
         public event Action<IWorldStateModule> OnWorldStateSyncableRegistered;
         public event Action<IWorldStateModule> OnWorldStateSyncableDeregistered;
 
+        private bool _dontShowRegErrors = false;
+        private List<string> _clashedIDs = new();
+        private bool _waitingToShowErrors = false;
+
         public void RegisterWorldStateSyncable(IWorldStateModule worldStateSyncable)
         {
+            if (WorldStateSyncables.ContainsKey(worldStateSyncable.ID))
+            {
+                Debug.LogError($"WorldStateSyncable with ID {worldStateSyncable.ID} is already registered.");
+                _clashedIDs.Add(worldStateSyncable.ID);
+
+#if UNITY_EDITOR
+                if (!_dontShowRegErrors && !_waitingToShowErrors)
+                {
+                    _waitingToShowErrors = true;
+
+                    //Delay so we have time to catch all syncables trying to register
+                    DOVirtual.DelayedCall(0.5f, () =>
+                    {
+                        _waitingToShowErrors = false;
+
+                        // Display the dialog and capture the button pressed
+                        _dontShowRegErrors = UnityEditor.EditorUtility.DisplayDialog(
+                            "VE2 Syncable Registration Error",
+                            $"Multiple networked VE2 components have the same gameobject name, this is not allowed\n\n" +
+                            $"Clashing IDs: \n -{string.Join("\n -", _clashedIDs)}\n\n" +
+                            "Please ensure that all WorldStateSyncables have unique IDs.",
+                            "Don't show again",
+                            "Keep reminding me");
+
+                        _clashedIDs.Clear();
+                    });
+                }
+#endif
+
+                return;
+            }
+
             WorldStateSyncables.Add(worldStateSyncable.ID, worldStateSyncable);
             OnWorldStateSyncableRegistered?.Invoke(worldStateSyncable);
         }
