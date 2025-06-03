89bc2059a004d4bf45d50302d0091a8f590de2b3
Simon Pocock <32938450+srpocock@users.noreply.github.com>
2025-06-03 22:02:00 +0100

    Add a little tester

---
+++ Virtual-Experience-Engine/Assets/FRAMEWORK/Private/DevStuff/ForceGrabTester.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using VE2.Core.VComponents.API;
+
+public class ForceGrabTester : MonoBehaviour
+{
+    [SerializeField] private GameObject Grabbable;
+    private IV_FreeGrabbable _grabbable => Grabbable.GetComponent<IV_FreeGrabbable>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.digit1Key.wasPressedThisFrame)
+            _grabbable.ForceLocalGrab(true);
+        if (Keyboard.current.digit2Key.wasPressedThisFrame)
+            _grabbable.ForceLocalGrab(false);
+        if (Keyboard.current.digit3Key.wasPressedThisFrame)
+            _grabbable.ForceLocalDrop();
+        if (Keyboard.current.digit4Key.wasPressedThisFrame)
+            _grabbable.UnlockLocalGrab();
+    }
+}
