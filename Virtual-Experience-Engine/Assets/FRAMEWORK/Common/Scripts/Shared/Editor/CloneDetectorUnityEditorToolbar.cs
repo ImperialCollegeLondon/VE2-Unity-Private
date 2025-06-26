7f847f9d6e1f7ad488e804ff601d1ea6626c5fd0
sventure <sidswamy2002@gmail.com>
2025-06-26 16:17:27 +0100

    warning for clone

---
+++ Virtual-Experience-Engine/Assets/FRAMEWORK/Common/Scripts/Shared/Editor/CloneDetectorUnityEditorToolbar.cs
@@ -0,0 +1,34 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using Toolbox.Editor;
+
+namespace VE2.Common.Shared.Editor
+{   
+    [InitializeOnLoad]
+    public static class CloneDetectorUnityEditorToolbar
+    {
+        static CloneDetectorUnityEditorToolbar()
+        {
+            ToolboxEditorToolbar.OnToolbarGuiRight += OnToolbarGui;
+        }
+
+        private static void OnToolbarGui()
+        {
+            GUILayout.FlexibleSpace();
+
+            string projectName = Application.productName;
+            string projectRoot = System.IO.Directory.GetParent(Application.dataPath).FullName;
+            string folderName = System.IO.Path.GetFileName(projectRoot);
+
+            if (!string.IsNullOrEmpty(folderName) && System.Text.RegularExpressions.Regex.IsMatch(folderName, @"_clone_\d+$"))
+            {
+                GUIStyle warningLabel = new GUIStyle(EditorStyles.boldLabel);
+                warningLabel.normal.textColor = Color.red;
+                warningLabel.fontSize = EditorStyles.label.fontSize - 1;
+                GUILayout.Label($"This is a clone of {projectName}. Don't save changes here!", warningLabel);
+            }
+        }
+    }
+}
+#endif
