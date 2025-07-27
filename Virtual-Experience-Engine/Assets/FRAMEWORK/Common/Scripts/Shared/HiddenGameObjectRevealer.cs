aff71e7da6511f9be9806c9963a38d60b2129486
Fred Tovey-Ansell <freddyta99@gmail.com>
2025-07-28 00:06:41 +0100

    Add hidden GO revealer

---
+++ Virtual-Experience-Engine/Assets/FRAMEWORK/Common/Scripts/Shared/HiddenGameObjectRevealer.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace VE2.Common.Shared
+{
+    internal static class HiddenGORevealerMenu
+    {
+        [MenuItem("VE2/Misc/Reveal Hidden GameObjects")]
+        private static void RevealHiddenGameObjects()
+        {
+            // Find all GameObjects in the current scenes, including inactive and hidden
+            GameObject[] allGameObjects = Object.FindObjectsOfType<GameObject>(true);
+
+            List<GameObject> hiddenGameObjects = new();
+
+            foreach (GameObject go in allGameObjects)
+            {
+                if (go.hideFlags != HideFlags.None)
+                {
+                    go.hideFlags = HideFlags.None;
+                    hiddenGameObjects.Add(go);
+                }
+            }
+
+            // Refresh the hierarchy view so changes become visible
+            EditorApplication.RepaintHierarchyWindow();
+
+            Debug.Log($"Revealed {hiddenGameObjects.Count} hidden GameObject(s).");
+            if (hiddenGameObjects.Count > 0)
+            {
+                Debug.Log("Hidden GameObjects revealed: " + string.Join(", ", hiddenGameObjects.ConvertAll(go => go.name)));
+            }
+            else
+            {
+                Debug.Log("No hidden GameObjects found.");
+            }
+        }
+    }
+}
