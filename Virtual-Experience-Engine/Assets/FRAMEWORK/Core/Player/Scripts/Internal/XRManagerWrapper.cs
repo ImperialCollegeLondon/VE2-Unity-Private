ae2ff5d8dd32213bf0a114e9a7ca0d93feca31f8
Fred Tovey-Ansell <freddyta99@gmail.com>
2025-07-30 17:34:35 +0100

    Error handling for XRManagerWrapper

--- Virtual-Experience-Engine/Assets/FRAMEWORK/Core/Player/Scripts/Internal/XRManagerWrapper.cs
+++ Virtual-Experience-Engine/Assets/FRAMEWORK/Core/Player/Scripts/Internal/XRManagerWrapper.cs
@@ -23,9 +23,20 @@ namespace VE2.Core.Player.Internal
 
         public event Action OnLoaderInitialized;
 
-        public void InitializeLoader() => StartCoroutine(nameof(InitializeLoaderAsync));
-        
-        private IEnumerator InitializeLoaderAsync() 
+        public void InitializeLoader()
+        {
+            try
+            {
+                StartCoroutine(nameof(InitializeLoaderAsync));
+            }
+            catch (Exception e)
+            {
+                if (Application.isEditor)
+                    Debug.LogError($"Failed to initialize XR Loader: {e.Message}");
+            }
+
+        }
+        private IEnumerator InitializeLoaderAsync()
         {
             Debug.Log("Initializing XR Loader");
             yield return XRManagerSettings.InitializeLoader();
@@ -33,11 +44,47 @@ namespace VE2.Core.Player.Internal
             Debug.Log("XR Loader init complete");
         }
 
-        public void DeinitializeLoader() => XRManagerSettings.DeinitializeLoader();
+        public void DeinitializeLoader()
+        {
+            try
+            {
+                XRManagerSettings.DeinitializeLoader();
+                Debug.Log("XR Loader deinitialized");
+            }
+            catch (Exception e)
+            {
+                if (Application.isEditor)
+                    Debug.LogError($"Failed to deinitialize XR Loader: {e.Message}");
+            }
+        }
         public XRLoader ActiveLoader => XRManagerSettings.activeLoader;
         public bool IsInitializationComplete => XRManagerSettings.isInitializationComplete;
-        public void StartSubsystems() => XRManagerSettings.StartSubsystems();
-        public void StopSubsystems() => XRManagerSettings.StopSubsystems();
+        public void StartSubsystems()
+        {
+            try
+            {
+                XRManagerSettings.StartSubsystems();
+            }
+            catch (Exception e)
+            {
+                if (Application.isEditor)
+                    Debug.LogError($"Failed to start XR subsystems: {e.Message}");
+                return;
+            }
+        }
+        public void StopSubsystems()
+        {
+            try
+            {
+                XRManagerSettings.StopSubsystems();
+            }
+            catch (Exception e)
+            {
+                if (Application.isEditor)
+                    Debug.LogError($"Failed to stop XR subsystems: {e.Message}");
+                return;
+            }
+        }
 
         private void Awake()
         {
